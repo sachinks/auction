95bd8b0120c64c239bc0b0d7221a962f884c2443
sachinks <sachin.kolige@gmail.com>
2026-03-14 20:16:28 +0000

    user creation

--- build.sh
+++ build.sh
@@ -3,11 +3,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py shell -c "
-from django.contrib.auth.models import User
-if not User.objects.filter(username='sk').exists():
-    User.objects.create_superuser('sk', 'sk@kpl.com', 'kpl2025')
-    print('Superuser created')
-else:
-    print('Superuser already exists')
-"
